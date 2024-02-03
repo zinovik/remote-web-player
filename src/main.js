9b45a8b96b1dda7c80152d69d6decbc4497b7356
Max Zinovik <zinovik@gmail.com>
2024-02-03 12:22:47 +0100

    refactoring

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const { exec } = require("child_process");
+const fs = require("fs");
 const { promisify } = require("util");
 const express = require("express");
 const ngrok = require("ngrok");
@@ -30,13 +31,14 @@ const runCommand = async (command, signal) => {
 
 const globalMutableState = {
   files: [],
+  page: "",
   currentController: null,
   currentSongIndex: 0,
   currentVolume: null,
 };
 
 const getTimeAndGenre = (path) => {
-  return new Promise((resolve, reject) =>
+  return new Promise((resolve) =>
     musicMetadata
       .parseFile(path)
       .then((metadata) =>
@@ -51,12 +53,13 @@ const getTimeAndGenre = (path) => {
             ),
         })
       )
-      .catch(() =>
+      .catch(() => {
+        console.warn("Error reading metadata", path);
         resolve({
           genre: "unknown",
           duration: 0,
-        })
-      )
+        });
+      })
   );
 };
 
@@ -72,142 +75,80 @@ const getTimeAndGenre = (path) => {
       .map(async (filePath) => {
         const path = filePath.substring(filePath.indexOf(SOURCE_PATH));
         const filename = path.replace(`${SOURCE_PATH}/`, "");
-        const [artist, yearAndAlbum, song] = filename.split("/");
+        const [artist, yearAndAlbum, trackAndTitle] = filename.split("/");
         const year = yearAndAlbum.substring(0, 4);
-        const album = yearAndAlbum.substring(6, yearAndAlbum.length);
-        const songNumber = song.substring(0, 2);
+        const album = yearAndAlbum.substring(7, yearAndAlbum.length);
+        const track = trackAndTitle.substring(0, 2);
+        const title = trackAndTitle
+          .substring(5, trackAndTitle.length)
+          .replace(".mp3", "");
 
         return {
           path,
           artist,
           year,
           album,
-          song: song.replace(".mp3", ""),
-          isFirstAlbumSong: songNumber === "01",
+          track,
+          title,
+          isFirstAlbumSong: track === "01",
           ...(await getTimeAndGenre(path)),
         };
       })
   );
 
+  console.log("Scanning done");
+
+  globalMutableState.page = fs
+    .readFileSync(`${__dirname}/index.html`)
+    .toString()
+    .replace(
+      "<div>SONG LIST PLACEHOLDER</div>",
+      globalMutableState.files
+        .map(
+          (
+            {
+              artist,
+              year,
+              album,
+              track,
+              title,
+              duration,
+              genre,
+              isFirstAlbumSong,
+            },
+            index
+          ) => {
+            let result = "";
+
+            if (isFirstAlbumSong)
+              result += `<hr /><div>${artist} - ${album} (${year})</div><div>${genre}</div><hr />`;
+
+            result += `<div onClick="handleSongClick(${index})" style="cursor: pointer;" id="song-${index}">${duration} | ${track} - ${title}</div>`;
+
+            return result;
+          }
+        )
+        .join("")
+    )
+    .replace(
+      "const files = [];",
+      `const files = ${JSON.stringify(
+        globalMutableState.files.map((file) => {
+          const { path, ...rest } = file;
+          return rest;
+        })
+      )};`
+    );
+
   const url = await ngrok.connect({ addr: PORT });
   console.log(url);
 })();
 
-const getPage = () => `<!DOCTYPE html>
-<html>
-  <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  </head>
-
-  <body>
-  <script>
-    let lastSongIndex;
-
-    const scrollToTheCurrentSong = (songElement) => {
-      (songElement || document.getElementById('song-' + lastSongIndex)).scrollIntoView({ block: "center" });
-    };
-
-    const updateCurrentInfo = (currentSongIndex, currentVolume) => {
-      if (currentSongIndex !== lastSongIndex) {
-        const songElement = document.getElementById('song-' + currentSongIndex);
-        if (songElement) {
-          songElement.style.color = 'blue';
-          scrollToTheCurrentSong(songElement);
-          const currentSong = songElement.innerHTML;
-          const currentSongElement = document.getElementById('current-song');
-          if (currentSongElement) currentSongElement.innerHTML = 'Current song: ' + currentSong;
-        }
-
-        const lastSongElement = document.getElementById('song-' + lastSongIndex);
-        if (lastSongElement) lastSongElement.style.color = 'black';
-        lastSongIndex = currentSongIndex;
-      }
-
-      const currentVolumeElement = document.getElementById('current-volume');
-      if (currentVolumeElement) currentVolumeElement.innerHTML = 'Current volume: ' + currentVolume;
-    };
-
-    const handleSongClick = async (index, element) => {
-      const response = await fetch('/play/' + index);
-      const { currentSongIndex, currentVolume } = await response.json();
-      updateCurrentInfo(currentSongIndex, currentVolume);
-    }
-
-    const handleVolumeClick = async (volume) => {
-      const response = await fetch('/volume/' + volume);
-      const { currentSongIndex, currentVolume } = await response.json();
-      updateCurrentInfo(currentSongIndex, currentVolume);
-    };
-
-    const handleClick = async (path) => {
-      const response = await fetch('/' + path);
-      const { currentSongIndex, currentVolume } = await response.json();
-      updateCurrentInfo(currentSongIndex, currentVolume);
-    };
-
-    const getInfo = async () => {
-      const response = await fetch('/info');
-      const { currentSongIndex, currentVolume } = await response.json();
-      updateCurrentInfo(currentSongIndex, currentVolume);
-    };
-
-    setTimeout(() => getInfo(), 1000);
-  </script>
-
-  <div style="position: fixed; background-color: lightgrey">
-    <div id="current-song">Current song: </div>
-    <div id="current-volume">Current volume: </div>
-    <button onclick="handleClick('play')">PLAY</button>
-    <button onclick="handleClick('stop')">STOP</button>
-    <button onclick="handleClick('random')">RANDOM</button>
-    <button onclick="handleClick('next')">NEXT</button>
-    <button onclick="handleClick('previous')">PREVIOUS</button>
-    <button onclick="handleVolumeClick(0)">Volume 0%</button>
-    <button onclick="handleVolumeClick(10)">Volume 10%</button>
-    <button onclick="handleVolumeClick(20)">Volume 20%</button>
-    <button onclick="handleVolumeClick(30)">Volume 30%</button>
-    <button onclick="handleVolumeClick(40)">Volume 40%</button>
-    <button onclick="handleVolumeClick(50)">Volume 50%</button>
-    <button onclick="handleVolumeClick(60)">Volume 60%</button>
-    <button onclick="handleVolumeClick(70)">Volume 70%</button>
-    <button onclick="handleVolumeClick(80)">Volume 80%</button>
-    <button onclick="handleVolumeClick(90)">Volume 90%</button>
-    <button onclick="handleVolumeClick(100)">Volume 100%</button>
-    <button onclick="scrollToTheCurrentSong()">Scroll to the current song</button>
-  </div>
-
-  <div style="height: 170px;"></div>
-
-  ${globalMutableState.files
-    .map(
-      (
-        { artist, year, album, song, duration, genre, isFirstAlbumSong },
-        index
-      ) => {
-        const results = [];
-
-        if (isFirstAlbumSong) {
-          results.push(
-            `<hr /><div>${artist}</div><div>${album}</div><div>${year}</div><div>${genre}</div><hr />`
-          );
-        }
-
-        results.push(
-          `<div onClick="handleSongClick(${index})" style="cursor: pointer;" id="song-${index}">${duration} | ${song}</div>`
-        );
-
-        return results.join("");
-      }
-    )
-    .join("")}
-  </body>
-</html>`;
-
 const stopSong = () => {
-  if (globalMutableState.currentController) {
-    console.log("🟥 STOP SONG");
-    globalMutableState.currentController.abort();
-  }
+  if (!globalMutableState.currentController) return;
+
+  console.log("🟥 STOP SONG");
+  globalMutableState.currentController.abort();
 };
 
 const startSong = async () => {
@@ -244,7 +185,7 @@ const setVolume = async () => {
 const app = express();
 
 app.get("/", (_, res) => {
-  res.send(getPage());
+  res.send(globalMutableState.page);
 });
 
 app.get("/volume/:volume", async (req, res) => {
@@ -323,7 +264,7 @@ app.get("/previous", (_req, res) => {
 });
 
 app.get("/info", (_req, res) => {
-  console.log("🔵 INFO REQUEST", globalMutableState.currentSongIndex);
+  console.log("🔵 INFO REQUEST");
   res.send({
     currentSongIndex: globalMutableState.currentSongIndex,
     currentVolume: globalMutableState.currentVolume,
